a4a28352520e00f9ee1a71cc129910a65122ba75
Bao Nguyen Hoai <baonguyen081296@gmail.com>
2021-09-27 17:32:30 +0700

    config

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,26 +1,28 @@
 export const path = {
-    HOME: '/',
-    LOGIN: '/login',
-    LOG_OUT: '/logout',
-    SYSTEM: '/system'
+  system: {},
+  HOME: '/',
+  HOME_PAGE: '/home',
+  LOGIN: '/login',
+  LOG_OUT: '/logout',
+  SYSTEM: { SYSTEM: '/system', USER_MANAGE: '/system/user-mange' },
 };
 
 export const languages = {
-    VI: 'vi',
-    EN: 'en'
+  VI: 'vi',
+  EN: 'en',
 };
- 
+
 export const manageActions = {
-    ADD: "ADD",
-    EDIT: "EDIT",
-    DELETE: "DELETE"
+  ADD: 'ADD',
+  EDIT: 'EDIT',
+  DELETE: 'DELETE',
 };
 
 export const dateFormat = {
-    SEND_TO_SERVER: 'DD/MM/YYYY'
+  SEND_TO_SERVER: 'DD/MM/YYYY',
 };
 
 export const YesNoObj = {
-    YES: 'Y',
-    NO: 'N'
-}
+  YES: 'Y',
+  NO: 'N',
+};
